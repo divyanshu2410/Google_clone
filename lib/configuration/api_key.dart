a09cdb59083763d227a6e1d19c7d632d79830ee9
divyanshu2410 <141527068+divyanshu2410@users.noreply.github.com>
2023-09-01 23:38:51 +0530

    Update api_key.dart

--- lib/configuration/api_key.dart
+++ lib/configuration/api_key.dart
@@ -1,2 +1,2 @@
-const String apiKey = "AIzaSyBxbTQBwTqrKkssae2i_-LaoS1m69uTg5A";
-const String contextKey = "963aceaeae38d43f8";
+const String apiKey = "api key";
+const String contextKey = "context key";
